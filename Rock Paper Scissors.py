f6e865d365a33da436ae9698cd0826a1d546c1da
Waszabii1 <148143917+Waszabii1@users.noreply.github.com>
2024-01-29 20:43:10 +0000

    Update Rock Paper Scissors.py

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -1,31 +1,92 @@
 import random
-while True:
-    choices = ["Rock", "Paper", "Scissors"]
+choices = {"R":"Rock", "P":"Paper", "S":"Scissors"}
+from colorama import Fore, init
 
-    computer = random.choice(choices)
-    player = None
+init(autoreset=True)
 
-    while player not in choices:
-        player = input("Rock, Paper, or Scissors?: ").capitalize()
+
+
+def score_shower(comp_score, play_score):
+    computer_score = comp_score
+    player_score = play_score
+    print(f"Player score: {player_score}. \nComputer score: {computer_score}")
+
+def game(comp_score, play_score):
+    computer = random.choice(list(choices.values()))
+    computer_score = comp_score
+    player_score = play_score
+    while True:
+        print(Fore.RED + "Rock (R), Paper (P), or Scissors (S)?")
+        choice = input().upper()
+        if choice == "R":
+            player = choices[str(choice)]
+            break
+        elif choice == "P":
+            player = choices[str(choice)]
+            break
+        elif choice == "S":
+            player = choices[str(choice)]
+            break
+        else:
+            print("Please put in valid input")
+            continue
 
     if player == computer:
         print("Computer: ", computer)
         print("Player: ",player)
         print("Tie!")
-    elif player == "Rock" and computer != "Rock":
+        score_shower(computer_score, player_score)
+
+    elif player == "Rock":
+        if computer == "Paper":
             print("Computer: ", computer)
             print("Player: ", player)
-            print("You lost :(")
-    elif player == "Scissors" and computer != "Scissors":
+            print("You lost")
+            computer_score += 1
+            score_shower(computer_score, player_score)
+        if computer == "Scissors":
             print("Computer: ", computer)
             print("Player: ", player)
-            print("You lost :(")
-    elif player == "Paper" and computer != "Paper":
+            print("You won")
+            player_score += 1
+            score_shower(computer_score, player_score)
+    elif player == "Paper":
+        if computer == "Scissors":
             print("Computer: ", computer)
             print("Player: ", player)
-            print("You win! :)")
+            print("You lost")
+            computer_score += 1
+            score_shower(computer_score, player_score)
+        if computer == "Rock":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("You won")
+            player_score += 1
+            score_shower(computer_score, player_score)
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("You lost")
+            computer_score += 1
+            score_shower(computer_score, player_score)
+        if computer == "Paper":
+            print("Computer: ", computer)
+            print("Player: ", player)
+            print("You won")
+            player_score += 1
+            score_shower(computer_score, player_score)
 
     play_again = input("Play again? (Y/N): ").upper()
 
-    if play_again != "Y":
-        break
+    if play_again == "Y":
+        game(computer_score, player_score)
+    else:
+        score_shower(computer_score, player_score)
+        print("See you next time!")
+        
+if __name__ == "__main__":
+    comp_score = 0
+    play_score = 0
+    game(comp_score, play_score)
+        
